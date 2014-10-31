ca5883f1307710c7045e96a6dcd2a31723b06c32
James Chong <jamesjychong@gmail.com>
2014-11-01 01:45:37 +0900

    Initial Commit

---
+++ JYMediaFramework/JYMediaFramework.h
@@ -0,0 +1,19 @@
+//
+//  JYMediaFramework.h
+//  JYMediaFramework
+//
+//  Created by Jae Yoon Chong on 11/1/14.
+//  Copyright (c) 2014 jamesjychong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JYMediaFramework.
+FOUNDATION_EXPORT double JYMediaFrameworkVersionNumber;
+
+//! Project version string for JYMediaFramework.
+FOUNDATION_EXPORT const unsigned char JYMediaFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JYMediaFramework/PublicHeader.h>
+
+
